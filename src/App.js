8ae4daa668175054359f33fcff44e774e5d64b20
ManonTfx <manon.trefoux@gmail.com>
2021-06-17 09:23:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { Project1, Project2, Project3, Project4 } from './pages/Projects';
+import Work from './pages/Work';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Work}/>
+      <Route exact path="/a-propos" component={About}/>
+      <Route exact path="/contact" component={Contact}/>
+      <Route exact path="/projet-1" component={Project1}/>
+      <Route exact path="/projet-2" component={Project2}/>
+      <Route exact path="/projet-3" component={Project3}/>
+      <Route exact path="/projet-4" component={Project4}/>
+    </Switch>
   );
 }
 
---
+++ src/pages/Work.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <div>
+            Work Home
+        </div>
+    );
+};
+
+export default Work;
