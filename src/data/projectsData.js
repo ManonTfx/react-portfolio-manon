2b983850b71821f368f521e9cab8119559cb317a
ManonTfx <manon.trefoux@gmail.com>
2021-06-17 12:18:02 +0200

    add projectsData.js

---
+++ src/data/projectsData.js
@@ -0,0 +1,34 @@
+export const projectsData = [
+    {
+      id: 1,
+      title: "@WEEM",
+      techno: "PLATEFORME COLLABORATIVE - PHP / WORDPRESS",
+      infos: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      img: "./assets/img/project-1.png",
+    },
+    {
+      id: 2,
+      title: "@BRUNA BEAUTEE",
+      techno: "SITE VITRINE  / WORDPRESS-ELEMENTOR",
+      infos:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      img: "./assets/img/project-2.png",
+    },
+    {
+      id: 3,
+      title: "@ZAAFIT",
+      techno: "CREATION DE SITE / SYMFONY / PHP /  TWIG / JS ",
+      infos:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      img: "./assets/img/project-3.png",
+    },
+    {
+      id: 4,
+      title: "@TODOLIST",
+      techno: "CREATION MINI SITE / JS / REACT",
+      infos:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      img: "./assets/img/project-4.png",
+    },
+  ];
+  
