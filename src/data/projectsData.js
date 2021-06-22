ec16c2b07e5323003ccca873742988f383d52f92
ManonTfx <manon.trefoux@gmail.com>
2021-06-22 17:57:11 +0200

    add id and url

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,34 +1,38 @@
 export const projectsData = [
     {
-      id: 1,
+      id: 0,
       title: "@WEEM",
       techno: "PLATEFORME COLLABORATIVE - PHP / WORDPRESS",
       infos: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
       img: "./assets/img/project-1.png",
+      url: "https://espace-client.weem.group/",
     },
     {
-      id: 2,
+      id: 1,
       title: "@BRUNA BEAUTEE",
       techno: "SITE VITRINE  / WORDPRESS-ELEMENTOR",
       infos:
         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
       img: "./assets/img/project-2.png",
+      url:"https://brunabeaute.fr/",
     },
     {
-      id: 3,
+      id: 2,
       title: "@ZAAFIT",
       techno: "CREATION DE SITE / SYMFONY / PHP /  TWIG / JS ",
       infos:
         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
       img: "./assets/img/project-3.png",
+      url:"https://zaafit.fr/",
     },
     {
-      id: 4,
+      id: 3,
       title: "@TODOLIST",
       techno: "CREATION MINI SITE / JS / REACT",
       infos:
         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
       img: "./assets/img/project-4.png",
+      url:"http://todolistdone.online/",
     },
   ];
   
