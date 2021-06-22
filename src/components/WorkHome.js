a1399e409f71d28f0ce6d5eb8bc12e327af9a2d3
ManonTfx <manon.trefoux@gmail.com>
2021-06-22 18:04:27 +0200

    add components WorkHome.js

---
+++ src/components/WorkHome.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { projectsData } from '../data/projectsData';
+
+const WorkHome = (props) => {
+    const [currentProject] = useState(projectsData);
+    const project = currentProject[props.projectNumber];
+
+    return (
+        <div className="projects-home">
+                <h3>{project.techno} <span>{project.title}</span></h3>
+                <button><a  target="_blank" className="hover" href={project.url}>voir le site</a></button>
+                <div className="img-container">
+                    <span>{project.infos}</span> 
+                    <img className="hover" src={project.img} alt={project.title} />
+                </div>
+        </div>
+    );
+};
+
+export default WorkHome;
