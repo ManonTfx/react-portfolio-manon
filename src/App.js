d6a1709c278db243fd7b8074dbe6ca39dee0968f
ManonTfx <manon.trefoux@gmail.com>
2021-06-22 18:01:17 +0200

    add Redirect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import { Project1, Project2, Project3, Project4 } from './pages/Projects';
 import Work from './pages/Work';
 
 
@@ -12,10 +11,7 @@ function App() {
       <Route exact path="/" component={Work}/>
       <Route exact path="/a-propos" component={About}/>
       <Route exact path="/contact" component={Contact}/>
-      <Route exact path="/projet-1" component={Project1}/>
-      <Route exact path="/projet-2" component={Project2}/>
-      <Route exact path="/projet-3" component={Project3}/>
-      <Route exact path="/projet-4" component={Project4}/>
+      <Redirect to="/"/>
     </Switch>
   );
 }
