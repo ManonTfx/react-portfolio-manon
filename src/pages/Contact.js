3ba632c3006b8f25d827c2d409a118aa3d350da6
ManonTfx <manon.trefoux@gmail.com>
2021-06-24 22:40:00 +0200

    add content and component ContactForm

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import ContactForm from '../components/ContactForm';
 import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
+import SocialNetwork from '../components/SocialNetwork';
+
 
 const Contact = () => {
+
     return (
-        <div>
+        <main className="contact">
             <Mouse />
             <Navigation />
-            Contact
-        </div>
+                <div className="contact-container">
+                    <div className="contact-content">
+                        <div className="phone">
+                            <div className="content">
+                                <h4>téléphone</h4>
+                                <p>0669044723</p>
+                            </div>
+                        </div>
+                        <div className="email">
+                            <div className="content">
+                                <h4>email</h4>
+                                <p>manon.trefoux@gmail.com</p>
+                            </div>
+                        </div>
+                        <SocialNetwork />
+                        </div>
+                <ContactForm />
+                </div>
+                    <div className="credits">
+                        <p>ManonTrefoux - 2021</p>
+                    </div>
+        </main>
     );
 };
 
