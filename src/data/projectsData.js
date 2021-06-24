6597fcf1675bfd18233602ff1c4cd3939f7a47bb
ManonTfx <manon.trefoux@gmail.com>
2021-06-24 22:38:31 +0200

    change infos projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,27 +1,27 @@
 export const projectsData = [
-    {
-      id: 0,
-      title: "@WEEM",
-      techno: "PLATEFORME COLLABORATIVE - PHP / WORDPRESS",
-      infos: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-      img: "./assets/img/project-1.png",
-      url: "https://espace-client.weem.group/",
-    },
+  {
+    id: 0,
+    title: "@BRUNA BEAUTEE",
+    techno: "SITE VITRINE  / WORDPRESS-ELEMENTOR",
+    infos:"Ce site a entièrement été conçu sur Wordpress avec Elementor. Il m'a été demandé par Bruna, une jeune maquilleuse qui souhaite avoir une visibilité sur internet. Nous avons travaillés ensemble le design et le SEO.",
+    img: "./assets/img/project-2.png",
+    url:"https://brunabeaute.fr/",
+  },
     {
       id: 1,
-      title: "@BRUNA BEAUTEE",
-      techno: "SITE VITRINE  / WORDPRESS-ELEMENTOR",
-      infos:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-      img: "./assets/img/project-2.png",
-      url:"https://brunabeaute.fr/",
+      title: "@WEEM",
+      techno: "PLATEFORME COLLABORATIVE - PHP / WORDPRESS",
+      infos: "Plateforme collaborative réalisé from scratch par laquelle WEEM peux échanger des documents de façon sécurisé avec leur client, LaPoste. J'ai effectué le design en reprenant leur charte graphique et mis en place les différentes fonctionnalités comme le partage de documents, la messagerie et les liens vers le CRM de WEEM (Hubspot). ",
+      alert:"IMPORTANT: Ce site étant sécurisé et strictement réservé à un usage interne, le lien ci-dessous ne fonctionnera pas ... ",
+      img: "./assets/img/project-1.png",
+      url: "",
     },
+    
     {
       id: 2,
       title: "@ZAAFIT",
       techno: "CREATION DE SITE / SYMFONY / PHP /  TWIG / JS ",
-      infos:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      infos:"Zaafit est mon projet de fin de formation grâce auquel j'ai obtenu mon titre professionnel de développeur d'applications multimédias. J'ai réalisé le design grâce au prototypage. Le site a entièrement été développé avec le langage PHP et son framework Symfony. Il contient un tableau de bord avec des courbes pour le suivi de poids, le calcul des calories et le suivi des repas. La base de donnée est stockée sur PhpMyAdmin.",
       img: "./assets/img/project-3.png",
       url:"https://zaafit.fr/",
     },
@@ -29,8 +29,7 @@ export const projectsData = [
       id: 3,
       title: "@TODOLIST",
       techno: "CREATION MINI SITE / JS / REACT",
-      infos:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      infos:"Mini site créé en ReactJs pour m'entraîner",
       img: "./assets/img/project-4.png",
       url:"http://todolistdone.online/",
     },
