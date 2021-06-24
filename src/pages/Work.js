1b8854d23fb1eb91d68c54b611f32e30e3d0cd91
ManonTfx <manon.trefoux@gmail.com>
2021-06-24 22:40:36 +0200

    update content

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
+import ScrollSmooth from '../components/ScrollSmooth';
+import SocialNetwork from '../components/SocialNetwork';
 import WorkHome from '../components/WorkHome';
 
 const Work = () => {
     return (
         <div className="works">
+            <ScrollSmooth />
            <Mouse />
            <Navigation />
            <div className="container">
-            <h2>Developpeuse <br /> Front / <br /> Creatrice de site Web </h2>
-            <p>Bonjour! Habitant Marseille depuis peu, je recherche une entreprise dans laquelle m’épanouir en tant qu’integratrice web ou developpeuse front.</p>
-            <div className="content-arrow-home">
-            <p> Mon travail  </p>
-            <img className="arrow-home hover" src={process.env.PUBLIC_URL + '/assets/img/arrow.png'} alt="flêche bas"/>
-            </div>
+                <h2>Developpeuse <br /> Front / <br /> Creatrice de site Web </h2>
+                <p>Bonjour👋 <br /> Habitant Marseille, je suis à la recherche d'une entreprise dans la région ou en remote dans le cadre de ma formation en alternance à la Wild Code School :) </p>
+                    <NavLink to="/a-propos">
+                        <button className="hover btn-home">Plus d'infos ...</button>
+                    </NavLink>
+                <SocialNetwork />
+                <div className="content-arrow-home">
+                    <p> Mon travail  </p>
+                    <a href="#projects"><img className="arrow-home hover" src={process.env.PUBLIC_URL + '/assets/img/arrow.png'} alt="flêche bas"/></a>
+                </div>
            </div>
+           <div id="projects">
            <WorkHome projectNumber={0}/>
            <WorkHome projectNumber={1}/>
            <WorkHome projectNumber={2}/>
            <WorkHome projectNumber={3}/>
+           </div>
         </div>
     );
 };
